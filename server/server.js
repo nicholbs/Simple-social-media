dd937b93b98ccf05323347bdd3b8908f497dc17a
June V. E. Hansen <jvhansen@stud.ntnu.no>
2020-10-08 14:34:27 +0200

    Added server connection in server.js

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@
 
 import express from 'express';
 import path from 'path';
+import mysql from 'mysql';
 const app = express();
-
 const PORT = 8081;
 
 app.listen(PORT, () => {
@@ -12,8 +12,32 @@ app.listen(PORT, () => {
 
 app.use(express.static(path.resolve() + '/server'));
 
+var db = mysql.createConnection({
+  host: "db",
+  user: "admin",
+  password: "password",
+  database: 'prog2053-proj'
+});
+
+db.connect(function (err) {
+  if (err) {
+    throw err;
+  }
+  console.log("Connected!");
+});
+
 app.get('/', (req, res) => {
   res.send("Hello world");
-  //res.send("./src/empty.js");
 })
 
+app.get('/getUsers', function (req, res) {
+  db.query('SELECT * FROM users', function (err, result) {
+    if (err) {
+      res.status(400).send('Error in database operation.');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(result));
+      //console.log("Result: " + res);
+    }
+  });
+});
