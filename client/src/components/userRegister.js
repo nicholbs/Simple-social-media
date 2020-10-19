780435246b463a73260e18d7f4acc03de46ef76c
Odd <oddhb@stud.ntnu.no>
2020-10-19 12:56:59 +0200

    Lagt til innhold i userRegister js og html

--- client/src/components/userRegister.js
+++ client/src/components/userRegister.js
@@ -1,1 +1,19 @@
 //LitElement for register page
+//LitElement for register page
+import { LitElement, html, css } from '/client/node_modules/lit-element/lit-element';
+
+export class RegisterPage extends LitElement {
+
+    static styles = css`
+    :host {
+        display: block;
+    }
+    `;
+
+    render() {
+        return html`
+        <div>Hello from RegisterPage!</div>
+        `;
+    }
+}
+customElements.define('register-page', RegisterPage);
